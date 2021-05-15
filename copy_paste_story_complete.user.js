61f022e7e55290cb4db8335103ba6144c9025d73
Adam Knights <knights.adam@googlemail.com>
2021-05-16 00:11:46 +0100

    Chnage to local/indexeddb storage

--- copy_paste_story_complete.user.js
+++ copy_paste_story_complete.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         Copy and paste story complete
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Adds button to copy a story on edit story page, works to paste on new story from 'add story'. TODO: copy story from view of a comic
 // @author       Adam Knights
-// @require      https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/localforage/1.9.0/localforage.min.js
 // @match        https://www.comics.org/story/revision/*/edit/*
 // @match        https://www.comics.org/issue/revision/*/add_story/*
 // @grant        none
 // ==/UserScript==
 
 /* eslint-env jquery */
-/* eslint-env js-cookie */
+/* esline-env localforage */
 
 // @require https://code.jquery.com/jquery-3.5.1.js
 
@@ -34,7 +34,7 @@ function featureHtml(featureId, position, name) {
     // this will add the buttons at the top of the form //
     $("div.edit").prepend('<button type="button" id="copy" style="margin-right: 10px"><i class="far fa-copy" style="color: blue"></i> Copy Data</button><button type="button" id="paste"><i class="fas fa-paste"  style="color: blue"></i> Paste Data</button>');
 
-    $("#copy").click(function (e) {
+    $("#copy").click(async function (e) {
         // this will save the form data, serialized as an array in a cookie named formData
         var nonCreatorObjectForm = $form.serializeArray().filter(f => f.name !== 'sequence_number' && f.name !== 'csrfmiddlewaretoken' && f.name !== 'feature_object' && !f.name.includes('story_credit_revisions') && f.name !== 'creator_help');
         var featureObjectForm = $form.serializeArray().filter(f => f.name === 'feature_object');
@@ -44,14 +44,13 @@ function featureHtml(featureId, position, name) {
 
         // Genre is a special case
         var genreObjectForm = $('#id_genre_to option').length === 0 ? '' : $('#id_genre_to option').map((i, el) => ({ t: $(el).attr('title'), v: $(el).attr('value') })).get();
-        console.log(genreObjectForm);
 
         for (let i = 0; i < featureObjectForm.length; i++) {
             featureObjectForm[i].friendlyFeatureName = $(`select[name="feature_object"] option[value="${featureObjectForm[i].value}"]`).text();
         }
 
-        // Need to make creator objects much smaller
         for (let i = 0; i < creatorObjectForm.length; i++) {
+            // Store friendly names for creator name and signatures
             if (creatorObjectForm[i].name.includes('-creator')) {
               creatorObjectForm[i].cn = $(`#id_${creatorObjectForm[i].name}`).next().find('.select2-selection__rendered').attr('title');
             }
@@ -60,117 +59,65 @@ function featureHtml(featureId, position, name) {
               creatorObjectForm[i].sn = $(`#id_${creatorObjectForm[i].name}`).next().find('[title="Remove all items"]').next().text();
             }
 
+            // Make creator objects much smaller
             creatorObjectForm[i].name = creatorObjectForm[i].name.replace('story_credit_revisions', 's_c_r');
             if (creatorObjectForm[i].value === "") {
               delete creatorObjectForm[i].value;
             }
         }
 
-        var formNCOJson = JSON.stringify(nonCreatorObjectForm);
-        if (formNCOJson.length > 4096) {
-          console.log(`Will not work as NCO form length > 4096. Length is ${formNCOJson.length}`);
-        }
-        Cookies.set('formDataNCO', formNCOJson, {
-            expires: 365
-        });
-
-        var formFOJson = JSON.stringify(featureObjectForm);
-        if (formFOJson.length > 4096) {
-          console.log(`Will not work as FO form length > 4096. Length is ${formFOJson.length}`);
-        }
-        Cookies.set('formDataFO', formFOJson, {
-            expires: 365
-        });
-
-        var formCOJson = JSON.stringify(creatorObjectForm);
-        if (formCOJson.length > 4096) {
-          console.log(`Will not work as FO form length > 4096. Length is ${formCOJson.length}`);
-        }
-        Cookies.set('formDataCO', formCOJson, {
-            expires: 365
-        });
-
-        if (genreObjectForm !== '') {
-          var formGenreJson = JSON.stringify(genreObjectForm);
-            console.log(formGenreJson);
-          if (formGenreJson.length > 4096) {
-              console.log(`Will not work as FO form length > 4096. Length is ${formGenreJson.length}`);
-          }
-          Cookies.set('formDataGenre', formGenreJson, {
-              expires: 365
-          });
-        }
+        await localforage.setItem('gcd_form_noncreator', nonCreatorObjectForm);
+        await localforage.setItem('gcd_form_featureobject', featureObjectForm);
+        await localforage.setItem('gcd_form_creatorobject', creatorObjectForm);
+        await localforage.setItem('gcd_form_genre', genreObjectForm);
     });
 
-    $("#paste").click(function (e) {
-        let $form_cookie_nco = Cookies.get('formDataNCO');
+    $("#paste").click(async function (e) {
 
-        if (!($form_cookie_nco === 'null' || $form_cookie_nco === undefined || $form_cookie_nco === null)) {
-            let $fields = JSON.parse($form_cookie_nco);
-
-            for (let i = 0; i < $fields.length; i++) {
-                let controlName = $fields[i].name;
-                let controlValue = $fields[i].value;
+        const gcd_form_noncreator = await localforage.getItem('gcd_form_noncreator');
+        if (gcd_form_noncreator) {
+            for (let i = 0; i < gcd_form_noncreator.length; i++) {
+                let controlName = gcd_form_noncreator[i].name;
+                let controlValue = gcd_form_noncreator[i].value;
 
                 $("#id_" + controlName).val(controlValue);
                 $("#wmd-input-id_" + controlName).val(controlValue);
             }
         }
 
-        let $form_cookie_fo = Cookies.get('formDataFO');
-
-        if (!($form_cookie_fo === 'null' || $form_cookie_fo === undefined || $form_cookie_fo === null)) {
-            let $fields = JSON.parse($form_cookie_fo);
-
-            for (let i = 0; i < $fields.length; i++) {
-                let controlName = $fields[i].name;
-                let controlValue = $fields[i].value;
-                let friendlyName = $fields[i].friendlyFeatureName;
-
-                let nextPosition = $('select[name="feature_object"] option').length + 1;
-                $('select[name="feature_object"]').append(featureHtml(controlValue, nextPosition, friendlyName));
+        const gcd_form_genre = await localforage.getItem('gcd_form_genre');
+        if (gcd_form_genre) {
+            for (let i = 0; i < gcd_form_genre.length; i++) {
+              $('#id_genre_to').append(`<option value="${gcd_form_genre[i].v}" title="${gcd_form_genre[i].t}">${gcd_form_genre[i].t}</option>`);
+              $(`#id_genre_from option[value="${gcd_form_genre[i].v}"]`).remove()
             }
         }
 
-        let $form_cookie_genre = Cookies.get('formDataGenre');
-
-        if (!($form_cookie_genre === 'null' || $form_cookie_genre === undefined || $form_cookie_genre === null)) {
-            let $fields = JSON.parse($form_cookie_genre);
-
-            for (let i = 0; i < $fields.length; i++) {
-              $('#id_genre_to').append(`<option value="${$fields[i].v}" title="${$fields[i].t}">${$fields[i].t}</option>`);
-              $(`#id_genre_from option[value="${$fields[i].v}"]`).remove()
-            }
-        }
-
-        let $form_cookie_co = Cookies.get('formDataCO');
-
-        if (!($form_cookie_co === 'null' || $form_cookie_co === undefined || $form_cookie_co === null)) {
-            let $fields = JSON.parse($form_cookie_co);
-
-            const totalFormsRequired = Number($fields.find(f => f.name === 's_c_r-TOTAL_FORMS').value);
+        let gcd_form_creatorobject = await localforage.getItem('gcd_form_creatorobject');
+        console.log(gcd_form_creatorobject);
+        if (gcd_form_creatorobject) {
+            const totalFormsRequired = Number(gcd_form_creatorobject.find(f => f.name === 's_c_r-TOTAL_FORMS').value);
             // First make the empty names tables
             for (let i = 1; i < totalFormsRequired; i++) {
-                //$('.formset_row-story_credit_revisions').last().after(creatorObjectHtml(i));
                 $('.add-row').click();
             }
 
-            for (let i = 0; i < $fields.length; i++) {
-                let controlName = $fields[i].name.replace('s_c_r', 'story_credit_revisions');
+            for (let i = 0; i < gcd_form_creatorobject.length; i++) {
+                let controlName = gcd_form_creatorobject[i].name.replace('s_c_r', 'story_credit_revisions');
 
-                let controlValue = $fields[i].value ?? "";
+                let controlValue = gcd_form_creatorobject[i].value ?? "";
 
                 if (controlName === 'story_credit_revisions-INITIAL_FORMS') {
                   controlValue = "0";
                 }
 
                 if (controlName.includes('-creator')) {
-                  $(`#id_${controlName}`).append(`<option value="${controlValue}" selected="" data-select2-id="7">${$fields[i].cn}</option>`);
+                  $(`#id_${controlName}`).append(`<option value="${controlValue}" selected="" data-select2-id="7">${gcd_form_creatorobject[i].cn}</option>`);
                     continue;
                 }
 
                 if (controlName.includes('-signature') && controlValue !== "") {
-                    $(`#id_${controlName}`).append(`<option value="${controlValue}" selected="" data-select2-id="42">${$fields[i].sn}</option>`);
+                    $(`#id_${controlName}`).append(`<option value="${controlValue}" selected="" data-select2-id="42">${gcd_form_creatorobject[i].sn}</option>`);
                 }
 
                 if (controlName.includes('-is_credited') || controlName.includes('-is_signed') || controlName.includes('uncertain')) {
@@ -184,6 +131,18 @@ function featureHtml(featureId, position, name) {
 
                 $("#id_" + controlName).val(controlValue);
                 $("#wmd-input-id_" + controlName).val(controlValue);
+            }
+        }
+
+        const gcd_form_featureobject = await localforage.getItem('gcd_form_featureobject');
+        if (gcd_form_featureobject) {
+            for (let i = 0; i < gcd_form_featureobject.length; i++) {
+                let controlName = gcd_form_featureobject[i].name;
+                let controlValue = gcd_form_featureobject[i].value;
+                let friendlyName = gcd_form_featureobject[i].friendlyFeatureName;
+
+                let nextPosition = $('select[name="feature_object"] option').length + 1;
+                $('select[name="feature_object"]').append(featureHtml(controlValue, nextPosition, friendlyName));
             }
         }
     });
