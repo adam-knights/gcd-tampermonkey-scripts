f7a033a85f0c5bec5add3217e9dee0a59ee1c869
Adam Knights <knights.adam@googlemail.com>
2021-05-16 07:26:50 +0100

    Fix issue where multiple migrate feature buttons on one page did not work

--- migrate-features-too.user.js
+++ migrate-features-too.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Migrate features too
 // @namespace    http://tampermonkey.net/
-// @version      0.2.1
+// @version      0.3.0
 // @description  Take editor text credits and move to creators
 // @author       Adam Knights
 // @match        https://www.comics.org/changeset/*/edit/
@@ -12,9 +12,8 @@
 
 // @require https://code.jquery.com/jquery-3.5.1.js
 
-function getMigrateButtonHtml(storyId) {
-    // <input type="hidden" name="csrfmiddlewaretoken" value="i3K25D3iNKfev3sFHSjynj7nt1Rskhy75xUV0BBZL51IxQqf9UaT46wNlTV6sSsw">
-    return `<form id="adam-inserted-migrate-${storyId}" class="story_button" method="POST" style="color: red" action="/story/revision/${storyId}/migrate/">
+function getMigrateButtonHtml(storyId, nextPos) {
+    return `<form id="adam-inserted-migrate-${storyId}-${nextPos}" class="story_button" method="POST" style="color: red" action="/story/revision/${storyId}/migrate/">
         <input type="submit" value="Migrate Feature" style="color: red">
       </form>`;
 }
@@ -24,14 +23,14 @@ function getMigrateButtonHtml(storyId) {
 
     let csrfCopy;
     if ($('.story_list').length >= 4) {
-        csrfCopy = $('.story_list').eq(3).find('[name="csrfmiddlewaretoken"]').eq(0).clone();
+        csrfCopy = $('.story_list').eq(3).find('[name="csrfmiddlewaretoken"]').eq(0);
     } else if ($('.story_list').length === 2) {
-        csrfCopy = $('.story_list').eq(1).find('[name="csrfmiddlewaretoken"]').eq(0).clone();
-        console.log(csrfCopy);
+        csrfCopy = $('.story_list').eq(1).find('[name="csrfmiddlewaretoken"]').eq(0);
     } else {
         return;
     }
 
+    let nextPos = 0;
     $('.story_list').eq(1).find('tr').each((index, element) => {
         const hasMigrateAlready = $(element).find('input[value="Migrate Credits"]').length === 1;
 
@@ -41,8 +40,9 @@ function getMigrateButtonHtml(storyId) {
 
         if (!hasMigrateAlready && !hasFeatureObjectAlready && !hasNoFeature) {
             const storyId = $(element).find('input[value="Edit"]').eq(0).attr('name').substring(5); // edit_ = 5 chars
-            $(element).find('td').last().append(getMigrateButtonHtml(storyId));
-            $(`#adam-inserted-migrate-${storyId}`).prepend(csrfCopy);
+            $(element).find('td').last().append(getMigrateButtonHtml(storyId, nextPos));
+            $(`#adam-inserted-migrate-${storyId}-${nextPos}`).prepend(csrfCopy.clone());
+            nextPos++;
         }
     });
 })();
