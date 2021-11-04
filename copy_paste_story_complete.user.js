bb852a44e12e6c009198d0fca5b39a5604cff1ef
Adam Knights <knights.adam@googlemail.com>
2021-11-04 00:15:10 +0000

    Copy paste story now works with character objects

--- copy_paste_story_complete.user.js
+++ copy_paste_story_complete.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Copy and paste story complete
 // @namespace    http://tampermonkey.net/
-// @version      0.3
-// @description  Adds button to copy a story on edit story page, works to paste on new story from 'add story'. TODO: character objects, copy story from view of a comic
+// @version      0.4
+// @description  Adds button to copy a story on edit story page, works to paste on new story from 'add story'. TODO: copy story from view of a comic
 // @author       Adam Knights
 // @require      https://cdnjs.cloudflare.com/ajax/libs/localforage/1.9.0/localforage.min.js
 // @match        https://www.comics.org/story/revision/*/edit/*
@@ -42,6 +42,9 @@ function featureHtml(featureId, position, name) {
         // Exclude fields we don't need to keep size down
         var creatorObjectForm = $form.serializeArray().filter(f => f.name.includes('story_credit_revisions') && f.name !== 'story_credit_revisions-MIN_NUM_FORMS' && f.name !== 'story_credit_revisions-MAX_NUM_FORMS' && !f.name.includes('-story_revision') && !f.name.includes('-id') && !f.name.includes('-DELETE'));
 
+        // We only pickup saved characters, not those in the appearing_characters select, those are ignored
+        var characterObjectForm = $form.serializeArray().filter(f => f.name.includes('story_character_revisions') && f.name !== 'story_character_revisions-MIN_NUM_FORMS' && f.name !== 'story_character_revisions-MAX_NUM_FORMS' && !f.name.includes('-story_revision') && !f.name.includes('-id') && !f.name.includes('-DELETE'));
+
         // Genre is a special case
         var genreObjectForm = $('#id_genre_to option').length === 0 ? '' : $('#id_genre_to option').map((i, el) => ({ t: $(el).attr('title'), v: $(el).attr('value') })).get();
 
@@ -66,9 +69,28 @@ function featureHtml(featureId, position, name) {
             }
         }
 
+        for (let i = 0; i < characterObjectForm.length; i++) {
+            // Store friendly name for character
+            if (characterObjectForm[i].name.includes('-character')) {
+              characterObjectForm[i].cn = $(`#id_${characterObjectForm[i].name}`).next().find('.select2-selection__rendered').attr('title');
+            }
+
+            // Store friendly name for group(s)
+            if (characterObjectForm[i].name.includes('-group')) {
+              characterObjectForm[i].gn = $(`select[name="${characterObjectForm[i].name}"] option[value="${characterObjectForm[i].value}"]`).text();
+            }
+
+            // Make character objects much smaller
+            characterObjectForm[i].name = characterObjectForm[i].name.replace('story_character_revisions', 's_c_r');
+            if (characterObjectForm[i].value === "") {
+              delete characterObjectForm[i].value;
+            }
+        }
+
         await localforage.setItem('gcd_form_noncreator', nonObjectForm);
         await localforage.setItem('gcd_form_featureobject', featureObjectForm);
         await localforage.setItem('gcd_form_creatorobject', creatorObjectForm);
+        await localforage.setItem('gcd_form_characterobject', characterObjectForm);
         await localforage.setItem('gcd_form_genre', genreObjectForm);
     });
 
@@ -139,10 +161,52 @@ function featureHtml(featureId, position, name) {
             }
         }
 
+        let gcd_form_characterobject = await localforage.getItem('gcd_form_characterobject');
+
+        if (gcd_form_characterobject) {
+            const totalFormsRequired = Number(gcd_form_characterobject.find(f => f.name === 's_c_r-TOTAL_FORMS').value);
+            // First make the empty characters tables
+            for (let i = 1; i < totalFormsRequired; i++) {
+                $('.add-row').last().click();
+            }
+
+            for (let i = 0; i < gcd_form_characterobject.length; i++) {
+                let controlName = gcd_form_characterobject[i].name.replace('s_c_r', 'story_character_revisions');
+
+                let controlValue = gcd_form_characterobject[i].value ?? "";
+
+                if (controlName === 'story_character_revisions-INITIAL_FORMS') {
+                    controlValue = "0";
+                }
+
+                if (controlName.includes('-character')) {
+                    $(`#id_${controlName} option`).removeAttr('selected')
+                    $(`#id_${controlName}`).append(`<option value="${controlValue}" selected="">${gcd_form_characterobject[i].cn}</option>`);
+                    continue;
+                }
+
+                if (controlName.includes('-group')) {
+                    $(`#id_${controlName}`).append(`<option value="${controlValue}" selected="">${gcd_form_characterobject[i].gn}</option>`);
+                    continue;
+                }
+
+                if (controlName.includes('-additional_information') || controlName.includes('-is_flashback') || controlName.includes('-is_origin') || controlName.includes('-is_death')) {
+                    if (controlValue === 'on') {
+                        $(`#id_${controlName}`).prop('checked', true).change();
+                    } else {
+                        $(`#id_${controlName}`).prop('checked', false);
+                    }
+                    continue;
+                }
+
+                $("#id_" + controlName).val(controlValue);
+                $("#wmd-input-id_" + controlName).val(controlValue);
+            }
+        }
+
         const gcd_form_featureobject = await localforage.getItem('gcd_form_featureobject');
         if (gcd_form_featureobject) {
             for (let i = 0; i < gcd_form_featureobject.length; i++) {
-                let controlName = gcd_form_featureobject[i].name;
                 let controlValue = gcd_form_featureobject[i].value;
                 let friendlyName = gcd_form_featureobject[i].friendlyFeatureName;
 
