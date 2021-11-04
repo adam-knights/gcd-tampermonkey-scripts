a1d0259e68ecdb945d70d1f639d2f8e96d6518c4
Adam Knights <knights.adam@googlemail.com>
2021-11-04 00:15:36 +0000

    Add new script for helping spot errors when migrating characters

---
+++ compare_characters_helper.js
@@ -0,0 +1,94 @@
+// ==UserScript==
+// @name         Compare characters helper
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Helper to show possible errors when migrating characters
+// @author       Adam Knights
+// @match        https://www.comics.org/changeset/*/compare/
+// @grant        none
+// ==/UserScript==
+
+/* eslint-env jquery */
+
+function getCountHtml(countLeft, countRight) {
+    let leftText = countLeft;
+    let rightText = countRight;
+    if (countLeft < countRight) {
+      rightText = `<span class="added">${countRight}</span>`;
+    }
+    else if (countLeft > countRight) {
+      rightText = `<span class="deleted">${countRight}</span>`;
+    }
+
+    return `<tr class="True"><td class="field_name">Char Count</td><td>${leftText}</td><td>${rightText}</td></tr>`;
+}
+
+function insertCompareHtml(insertBefore, thingsOnLeftNotInRight, thingsOnRightNotInLeft) {
+    if (thingsOnLeftNotInRight.length === 0 && thingsOnRightNotInLeft.length === 0) {
+      return;
+    }
+
+    let leftText = '';
+    let rightText = '';
+
+    if (thingsOnLeftNotInRight.length > 0) {
+        leftText = thingsOnLeftNotInRight.sort().join('<br>');
+    }
+
+    if (thingsOnRightNotInLeft.length > 0) {
+        rightText = thingsOnRightNotInLeft.sort().join('<br>');
+    }
+
+    insertBefore.before(`<tr class="True"><td class="field_name">Character Diffs</td><td>${leftText}</td><td>${rightText}</td></tr>`);
+    return;
+}
+
+function parse(str) {
+  let result = [], item = '', depth = 0;
+
+  function push() { if (item) result.push(item.trim()); item = ''; }
+
+  for (let i = 0; i < str.length; i++) {
+    const c = str[i]
+    if (!depth && c === ';') push();
+    else {
+      item += c;
+      if (c === '[' || c === '(') depth++;
+      if (c === ']' || c === ')') depth--;
+    }
+  }
+
+  push();
+  return result;
+}
+
+(async function() {
+    'use strict';
+
+    $('td:contains(Characters)').each(function() {
+        const left = $(this).next().text().trim();
+        const right = $(this).next().next().text().trim();
+        console.log(left);
+        console.log(right);
+
+        if (!left && !right) {
+          return;
+        }
+
+        const countLeft = 1 + (left.match(new RegExp(";", "g")) || []).length;
+        const countRight = 1 + (right.match(new RegExp(";", "g")) || []).length;
+        $(this).parent().before(getCountHtml(parseInt(countLeft), parseInt(countRight)));
+
+        const leftChars = parse(left);
+        const rightChars = parse(right);
+        console.log(leftChars);
+        console.log(rightChars);
+
+        const thingsOnLeftNotInRight = leftChars.filter(x => !rightChars.includes(x));
+        const thingsOnRightNotInLeft = rightChars.filter(x => !leftChars.includes(x));
+        console.log(thingsOnLeftNotInRight);
+        console.log(thingsOnRightNotInLeft);
+
+        insertCompareHtml($(this).parent(), thingsOnLeftNotInRight, thingsOnRightNotInLeft);
+    });
+})();
